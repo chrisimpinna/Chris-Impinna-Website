53df6f704b28561d69ad97559fd02108d68b89b9
DS9 <93636816+deepspaceIX@users.noreply.github.com>
2022-09-16 10:56:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-// Creates a new instance of the Github object exposed by Github.js
-var github = new Github({
-  username: 'deepspaceIX',
-  password: '382727aA',
-  auth: 'basic'
-});
- 
-// Creates an object representing the repository you want to work with
-var repository = github.getRepo('deepspaceIX', 'deepspace9');
- 
-// Creates a new file (or updates it if the file already exists)
-// with the content provided
-repository.write(
-   'main', // e.g. 'master'
-   'text.txt', // e.g. 'blog/index.md'
-   'shut up', // e.g. 'Hello world, this is my new content'
-   'shut up', // e.g. 'Created new index'
-   function(err) {}
-);
+function printRepoCount() {
+  var responseObj = JSON.parse(this.responseText);
+  console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
+}
+var request = new XMLHttpRequest();
+request.onload = printRepoCount;
+request.open('get', 'https://api.github.com/users/deepspaceIX', true)
+request.send()
