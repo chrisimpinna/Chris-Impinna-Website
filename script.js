22f45347b2fb1600c7b30e44b11d067189a9f9e3
Chris Impinna <93636816+chrisimpinna@users.noreply.github.com>
2022-11-11 10:14:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function printRepoCount() {
-  var responseObj = JSON.parse(this.commit.committer.date);
-  document.getElementById("last-updated").innerHTML = "NEW COMMIT" + responseObj;
-}
-var request = new XMLHttpRequest();
-request.onload = printRepoCount();
-request.open('get', 'https://api.github.com/repos/USER/REPOSITORY/commits', true)
-request.send()
-  document.getElementById("last-updated").innerHTML = "NEW COMMIT" + responseObj;
+window.addEventListener("load", function () {
+  function printRepoCount() {
+    var Element = document.getElementsByClassName("last-updated");
+    Element[0].innerHTML = "GeeksforGeeks";
+  }
+  
+  var request = new XMLHttpRequest();
+  request.onload = printRepoCount();
+  request.open('get', 'https://api.github.com/repos/USER/REPOSITORY/commits', true)
+  request.send()
+});
