448154adeb271f92edf4182c3cb619b32ef31525
Chris Impinna <93636816+chrisimpinna@users.noreply.github.com>
2022-11-11 10:45:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   function printRepoCount() {
     document.getElementsByClassName("last-updated").innerHTML = "GeeksforGeeks";
   }
-  
+  document.getElementsByClassName("last-updated").innerHTML = "GeeksforGeeks";
   var request = new XMLHttpRequest();
   request.onload = printRepoCount();
   request.open('get', 'https://api.github.com/repos/chrisimpinna/DS_IX-Website/commits', true)
