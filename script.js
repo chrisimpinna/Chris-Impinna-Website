6834509a2b3da73ab730475b34031587c75d2b20
Chris Impinna <93636816+chrisimpinna@users.noreply.github.com>
2022-11-12 14:29:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function getArchiveFiles() {
     let stop = 0;
     
     if (responseReceived.indexOf("path\":", lastIndex) != -1 && superStop == 0) {
-      for (let x = responseReceived.indexOf("path\":", lastIndex-2)+7; x<=responseReceived.length; x++) {
+      for (let x = responseReceived.indexOf("path\":", lastIndex-2)+8; x<=responseReceived.length; x++) {
         if (responseReceived[x] != "," && stop==0) {
           fileName = fileName + responseReceived[x];
           lastIndex = x
@@ -46,6 +46,7 @@ function getArchiveFiles() {
     };
   };
   
+  fileName = fileName.replace("\"", ",");
   if (document.getElementById("archive-title") != null) {
     document.getElementById("archive-title").innerHTML = fileName; 
   };    
