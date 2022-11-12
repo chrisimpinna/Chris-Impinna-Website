e0fc1168c00f8c5f05162208f0ba9a3fec232785
Chris Impinna <93636816+chrisimpinna@users.noreply.github.com>
2022-11-12 14:24:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getArchiveFiles() {
   for (let i = 0; i<=100; i++){
     let stop = 0;
     let superStop = 0
-    if (responseReceived.indexOf("path\":", lastIndex-2) != null && superStop == 0) {
+    if (responseReceived.indexOf("path\":", lastIndex-2) != undefined && superStop == 0) {
       for (let x = responseReceived.indexOf("path\":", lastIndex-2); x<=responseReceived.length; x++) {
         if (responseReceived[x] != "," && stop==0) {
           fileName = fileName + responseReceived[x];
