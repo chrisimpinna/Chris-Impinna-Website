723252bc45a49b79800116e10a422a15812672d3
Chris Impinna <93636816+chrisimpinna@users.noreply.github.com>
2022-11-11 21:44:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+document.onload = function(){};
 
+let newString = "Last updated: ";
+
+function getUpdateDate() {
+  let stop = 0;
+  var responseReceived = this.responseText;
+  let i = responseReceived.indexOf("date\":")
+  for (let e = i+10; e<i+40; e++) {
+    if (stop == 0 && responseReceived[e] != "Z") {
+      newString = newString+responseReceived[e];
+    } else {
+      stop = 1;
+    };
+  };
+
+  newString = newString.replace("T", " ")
+  document.getElementById("update").innerHTML = newString + " (GMT)"; 
+};
+
+var request = new XMLHttpRequest();
+request.onload = getUpdateDate;
+request.open('get', 'https://api.github.com/repos/chrisimpinna/DS_IX-Website/commits', true);
+request.send();
